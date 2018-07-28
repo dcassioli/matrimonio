ebbf170ae00272d2062e6b7ae3c713f833592c45
Davide Cassioli <dcassioli@gmail.com>
2018-07-28 09:41:21 +0200

    Broken build fix: added "babel-core" as a peer dependency; made all dependencies production ones in package.json.

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -40,7 +40,11 @@ gulp.task('scripts', () =>
       showFiles: true
     }))
     .pipe(when(argv.prod, rename({suffix: '.min'})))
-    .pipe(when(argv.prod, when('*.js', uglify({preserveComments: 'some'}))))
+    .pipe(when(argv.prod, when('*.js', uglify({
+      output: {
+        comments: 'some'
+      }
+    }))))
     .pipe(when(argv.prod, size({
       showFiles: true
     })))
